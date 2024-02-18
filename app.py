382111d7e71afc9cc61b6a8e6e3847aa5717f4d0
Brian <mwathibrian7@gmail.com>
2024-02-18 21:51:47 +0300

    Upload file validation

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask, jsonify, request
 from celery import Celery, Task
-from packages.pdfLoader.load_pdf import MpesaLoader
+from werkzeug.exceptions import RequestEntityTooLarge
 from tasks import extract_data_from_pdf
 from celery.result import AsyncResult
 from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'pdf'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def celery_init_app(app: Flask) -> Celery:
@@ -29,28 +36,42 @@ app.config.from_mapping(
     )
 )
 
+app.config['MAX_CONTENT_LENGTH'] = 1.5 * 1000 * 1000  # 1.5 Megabytes
+
 celery_app = celery_init_app(app)
 
 
 @app.post('/')
 def index():
-    if request.method == 'POST':
-        f = request.files['file']
-        password = request.form['password']
+    try:
+        if 'file' not in request.files:
+            return {'error': "No file uploaded"}, 400
 
-        fP = os.path.join('/tmp/', secure_filename(f.filename))
-        f.save(fP)
-        result = extract_data_from_pdf.delay(fP, password)
+        uploadedFile = request.files['file']
 
-        return jsonify({
-            'taskID': result.id
-        })
+        if uploadedFile.filename == '':
+            return {'error': 'No selected file'}, 400
+
+        if uploadedFile and allowed_file(uploadedFile.filename):
+            password = request.form['password']
+
+            fP = os.path.join('/tmp/', secure_filename(uploadedFile.filename))
+            uploadedFile.save(fP)
+            result = extract_data_from_pdf.delay(fP, password)
+
+            return jsonify({
+                'taskID': result.id
+            }), 202
+        else:
+            return {'error': 'Invalid file type'}, 400
+    except RequestEntityTooLarge:
+        return {'error': 'Request Entity Too Large'}, 413
 
 
 @app.get('/result/<id>')
 def task_result(id: str) -> dict[str, object]:
     result = AsyncResult(id)
-    if result.ready():
+    if result.ready() and result.successful():
         return result.result
     return {
         "ready": result.ready(),
